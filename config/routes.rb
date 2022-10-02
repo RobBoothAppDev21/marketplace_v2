3047e8d6d2e598ef2638aa54f58c0d75ce96bdc3
Rob Young <rhyoung214@gmail.com>
2022-10-02 15:59:22 -0700

    Add static pages controller, custom css

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'static_pages/home', to: 'static_pages#home', as: 'home'
+  get 'static_pages/help', to: 'static_pages#help', as: 'help'
+  get 'static_pages/about', to: 'static_pages#about', as: 'about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
